17c2d2fc026c1f6be94a7195116f327108135187
Seb Zanardo <sebzanardo@optusnet.com.au>
2025-01-31 05:59:50 +1100

    Cooked check

--- src/core/globals.py
+++ src/core/globals.py
@@ -6,6 +6,7 @@ default_settings = {
     "fullscreen": False,
     "vsync": True,
     "screenshake": True,
+    "cooked": False  # If this is loaded then assume rest is not cooked
 }
 
 settings = default_settings.copy()
