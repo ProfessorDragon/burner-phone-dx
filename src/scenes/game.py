4ecd307dd26a8cb703be88608b1a8664d9312ce8
Seb Zanardo <sebzanardo@optusnet.com.au>
2025-01-31 14:03:06 +1100

    IDK why but yes, now music  loaded volume is working COrreCTlY

--- src/scenes/game.py
+++ src/scenes/game.py
@@ -103,7 +103,8 @@ class Game(Scene):
         self.player = Player(_tile_size_vec(10.5, 12))
 
         self.camera = Camera.empty()
-        self.camera.offset = pygame.Vector2(c.WINDOW_WIDTH / 2, c.WINDOW_HEIGHT / 2)
+        self.camera.offset = pygame.Vector2(
+            c.WINDOW_WIDTH / 2, c.WINDOW_HEIGHT / 2)
         self.camera.motion.position = _camera_target(self.player)
 
         self.dialogue = DialogueSystem()
@@ -139,7 +140,8 @@ class Game(Scene):
             _add_timer(
                 self,
                 1.5,
-                lambda: dialogue_execute_script_scene(self.dialogue, "OPENING CALL 1"),
+                lambda: dialogue_execute_script_scene(
+                    self.dialogue, "OPENING CALL 1"),
             )
         if not dialogue_has_executed_scene(self.dialogue, "OPENING CALL 2"):
             _add_timer(self, 6, self.opening_call_2)
@@ -180,7 +182,8 @@ class Game(Scene):
 
         # update and render entities within this area
         decor_bounds = camera_rect(self.camera)
-        entity_bounds = decor_bounds.inflate(c.TILE_SIZE * 12, c.TILE_SIZE * 12)
+        entity_bounds = decor_bounds.inflate(
+            c.TILE_SIZE * 12, c.TILE_SIZE * 12)
 
         if not self.paused:
             if self.editor.enabled:
@@ -261,7 +264,8 @@ class Game(Scene):
                 for entity in self.entities:
                     path = entity.get_path()
                     if path:
-                        bound_rects = [pygame.Rect(point, (1, 1)) for point in path]
+                        bound_rects = [pygame.Rect(point, (1, 1))
+                                       for point in path]
                     else:
                         bound_rects = [entity.get_hitbox()]
                     if entity_bounds.collidelist(bound_rects) >= 0:
@@ -301,9 +305,11 @@ class Game(Scene):
 
         # render tiles within this area
         tile_bounds = pygame.Rect(
-            (self.camera.motion.position.x - self.camera.offset.x - self.camera.shake_offset.x)
+            (self.camera.motion.position.x -
+             self.camera.offset.x - self.camera.shake_offset.x)
             // c.TILE_SIZE,
-            (self.camera.motion.position.y - self.camera.offset.y - self.camera.shake_offset.y)
+            (self.camera.motion.position.y -
+             self.camera.offset.y - self.camera.shake_offset.y)
             // c.TILE_SIZE,
             surface.get_width() // c.TILE_SIZE,
             surface.get_height() // c.TILE_SIZE,
@@ -453,7 +459,8 @@ class Game(Scene):
             ):
                 # if trying to revoke comms, played shadowless tree, and declined the call, reset to intro
                 self.player.progression.main_story = MainStoryProgress.INTRO
-                dialogue_remove_executed_scene(self.dialogue, "SHADOWLESS TREE")
+                dialogue_remove_executed_scene(
+                    self.dialogue, "SHADOWLESS TREE")
 
         elif self.player.progression.main_story == MainStoryProgress.FINALE_NO_MOVEMENT:
             if not dialogue_has_executed_scene(self.dialogue, "FINALE FINAL"):
@@ -464,7 +471,8 @@ class Game(Scene):
                 fade_start(self.fade, False)
                 _add_timer(self, 0.1, self.finale_explosion)
                 _add_timer(self, 4, self.exit_to_credits)
-                dialogue_execute_script_scene(self.dialogue, "FINALE FADE OUT DONE")
+                dialogue_execute_script_scene(
+                    self.dialogue, "FINALE FADE OUT DONE")
 
 
 def _add_timer(scene: Game, duration: float, callback: Callable) -> Timer:
